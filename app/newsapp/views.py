fd831d7fbdc764a5e8464b64013c357897d1a737
odoyonina <odoyoninah@gmail.com>
2022-05-02 15:43:49 +0300

    modified __init__.py

---
+++ app/newsapp/views.py
@@ -0,0 +1,3 @@
+from flask import render_template
+from . import newsapp
+from ..request import get_news
--- app/request.py
+++
@@ -1,2 +0,0 @@
-top_headlines = newsapi.get_top_headlines(sources = 'bbc-news')
-t_articles = top_headlines['articles']
