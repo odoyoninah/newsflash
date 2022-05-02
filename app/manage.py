9f4f6a48cc28141bae268c7f90ddf0ef054df308
odoyonina <odoyoninah@gmail.com>
2022-05-02 20:22:56 +0300

    add run function

--- app/manage.py
+++ app/manage.py
@@ -3,3 +3,8 @@ from flask_script import Manager, Server
 
 app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server', Server)
+
+if __name__ == '__main__':
+    manager.run()
