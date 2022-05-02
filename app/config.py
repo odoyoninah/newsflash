795623533a0c15cdb28b845e953292b0e324959f
odoyonina <odoyoninah@gmail.com>
2022-05-02 20:16:20 +0300

    add class config

--- app/config.py
+++ app/config.py
@@ -8,9 +8,23 @@ class config:
     NEWS_API_BASE_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey={194f9337ee204d7990dd869f9cddc6c3}"
     NEWS_API_KEY = os.environ.get('194f9337ee204d7990dd869f9cddc6c3')
 
-class ProdConfig:
+class ProdConfig(config):
+
     """
     Production  configuration child class
     """
     pass
+
+class DevConfig(config):
+    """
+    Development  configuration child class
+    """
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
 
