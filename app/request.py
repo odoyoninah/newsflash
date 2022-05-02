63912ff0e5f54ce0a26e5753d07f08a2d6bb72d8
odoyonina <odoyoninah@gmail.com>
2022-05-02 18:18:42 +0300

    import urllib and api_key

---
+++ app/request.py
@@ -0,0 +1,11 @@
+import urllib.request,json
+from .models import News
+
+api_key = None
+base_url = None
+
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+
