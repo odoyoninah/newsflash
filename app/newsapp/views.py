4c9629cec1afa7a642f5b652cd1b711a68c012aa
odoyonina <odoyoninah@gmail.com>
2022-05-02 15:53:13 +0300

    define news index

--- app/newsapp/views.py
+++ app/newsapp/views.py
@@ -1,3 +1,15 @@
 from flask import render_template
 from . import newsapp
 from ..request import get_news
+
+@newsapp.route('/')
+def index():
+    
+    """
+    View root page function that returns the index page and its data
+    """
+
+    news = get_news()
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title, news = news)
+    
