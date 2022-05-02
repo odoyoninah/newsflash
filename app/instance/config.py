50de9a4d1e7268a80d13a9c762a01e43a7d98a68
odoyonina <odoyoninah@gmail.com>
2022-05-02 18:45:35 +0300

    add news dictonary

--- app/instance/config.py
+++ app/instance/config.py
@@ -1,1 +1,1 @@
-newsapi = NewsApiClient(api_key='194f9337ee204d7990dd869f9cddc6c3')
+NEWS_API_KEY = '194f9337ee204d7990dd869f9cddc6c3'
--- app/request.py
+++ app/request.py
@@ -26,3 +26,23 @@ def get_news(category):
             news_results = process_results(news_results_list)
 
     return news_results
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+    Args:
+        news_list: A list of dictionaries that contain news details
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        title = news_item.get('title')
+        description = news_item.get('description')
+        urlToImage = news_item.get('urlToImage')
+        content = news_item.get('content')
+        publishedAt = news_item.get('publishedAt')
+
+        news_object = News(title,description,urlToImage,content,publishedAt)
+        news_results.append(news_object)
+        
