dd9b8959e266651d1a40b2e141e789022e9c67d5
odoyonina <odoyoninah@gmail.com>
2022-05-02 18:28:28 +0300

    create News class

---
+++ app/model.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,title,description,urlToImage,content,publishedAt):
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.content = content
+        self.publishedAt = publishedAt
--- app/request.py
+++ app/request.py
@@ -1,5 +1,5 @@
 import urllib.request,json
-from .models import News
+from .model import News
 
 api_key = None
 base_url = None
@@ -9,3 +9,20 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
 
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_results(news_results_list)
+
+    return news_results
