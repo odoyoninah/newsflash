252ebeaf11d34b4e1896a6e3366112fc45ee2969
odoyonina <odoyoninah@gmail.com>
2022-05-03 17:54:43 +0300

    Patch:fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # from ensurepip import bootstrap
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import config_options
+from app.config import config_options
 
 bootstrap = Bootstrap()
 
